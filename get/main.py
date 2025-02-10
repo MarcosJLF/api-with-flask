4914d31776ad350e9dc1b45805b547a3eb2ea81a
ma060200 <ma060200@intelbras.com.br>
2025-02-10 16:20:25 -0300

    get

--- get/main.py
+++ get/main.py
@@ -4,17 +4,41 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 names = [
-    {'id':1, 'nome':"marcos júnior lemes ferreira"},
-    {'id':2, 'nome':"João"}
+    {'id':1, 'nome':"marcos"},
+    {'id':2, 'nome':"João"},
+    {'id':3, 'nome':"Maria"}
 ]
 
 
 @app.route('/names', methods=['GET'])
 def get_names():
-    return jsonify(names)
+    return jsonify(names), 200
 
 
+@app.route('/nomes/<int:id>', methods=['GET'])
+def get_namess(id):
 
+    name = next((name for name in names if name['id'] == id), None)
+
+    if name is not None:
+        return jsonify(name), 200
+    else:
+        return jsonify({'error':'name not found'}), 404
+    
+        
+
+@app.route('/nomes/c/<string:nome>', methods=['GET'])
+
+def get_nomee(nome):
+
+    print(nome)
+
+    nome = next((item for item in names if item['nome'].lower() == nome.lower()), None)
+
+    if nome is not None:
+        return jsonify(nome), 200
+    else:
+        return jsonify({'error':'name not found'}), 404
 
 if __name__=='__main__':
     app.run(debug=True)
